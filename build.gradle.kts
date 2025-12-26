54bddffd7f0fd55f2d9813c18500164c64e0355d
AdityaParmar <aditya@neomenta.com>
2025-12-26 12:57:57 +0530

    cosmetic

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    kotlin("plugin.serialization") version "1.9.22" apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt.android) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
 }
 val buildToolsVersion by extra("36.0.0")
