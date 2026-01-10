34827bf3b078a4074625c80e7bcc996b5dd2c009
AdityaParmar <aditya@neomenta.com>
2026-01-10 23:06:15 +0530

    api code sync

--- app/src/main/java/com/neome/api/home/base/dto/DtoMessagePayloadMessageDeleted.kt
+++ app/src/main/java/com/neome/api/home/base/dto/DtoMessagePayloadMessageDeleted.kt
@@ -5,9 +5,6 @@
 
 package com.neome.api.home.base.dto
 
-import com.neome.core.common.IPayload
-
 interface DtoMessagePayloadMessageDeleted : DtoMessagePayload {
-    val arr: List<IPayload>
-    val map: Map<String, IPayload>
+
 }
